bef5fc0467061b30c0ff8dbfc53090d3dfed60bd
devjoseph1992 <devjosephflorina@gmail.com>
2018-10-11 22:27:49 +0800

    installing passport and configure api

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -9,6 +9,15 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+        /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
     /**
      * Display a listing of the resource.
      *
@@ -54,6 +63,7 @@ class UserController extends Controller
         //
     }
 
+    
     /**
      * Update the specified resource in storage.
      *
